c7002e914409b7d22204acfb9b66d4a6544c0376
gian12z <gianluca.zancomiii@gmail.com>
2025-11-30 15:14:29 -0300

    pagina de perfil agregada

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ProductDetailPage from './pages/ProductDetailPage';
 import CartPage from './pages/CartPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import ProfilePage from './pages/ProfilePage';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/cart" element={<CartPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
     </Router>
   )
